b164ddda5e827e4bc47fbe4d12e375e32b4d336b
JManuel1981 <jmmelero.1981@gmail.com>
2023-08-30 11:13:10 +0200

    Pinterest Murciano

---
+++ PINTEREST/murciarest.js
@@ -0,0 +1,7 @@
+function like(elemento){
+    if(elemento.parentElement.parentElement.style.order="-1")
+    elemento.parentElement.parentElement.style.order= "0";
+    else elemento.parentElement.parentElement.style.order="-1";
+    elemento.classList.toggle("fa-solid");
+    elemento.classList.toggle("fa-regular");
+}
